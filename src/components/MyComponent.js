40efbb252456eb1486b145cf60bbb1fa04a2b466
Đinh Mạnh Tưởng <145707598+prism9x@users.noreply.github.com>
2023-12-29 00:17:06 +0700

    State Component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Manh Tuong',
+        address: 'Phu Xuyen',
+        age: 29
+    };
+
     render() {
         return (
             <div>
-                First Component
+                My name is: {this.state.name}
+                My age: {this.state.age}
+                My address: {this.state.address}
             </div>
         );
     }
