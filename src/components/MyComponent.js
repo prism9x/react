caed00ae26988a0aa63443ed86392cd0d3b8cc15
Đinh Mạnh Tưởng <145707598+prism9x@users.noreply.github.com>
2023-12-29 21:55:27 +0700

    using event in react

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,13 +8,22 @@ class MyComponent extends React.Component {
         age: 29
     };
 
+    handleClick(event) {
+        console.log('>> click me');
+    }
+    handleOnMouseOver(event) {
+        console.log('X', event.pageX)
+        console.log('Y', event.pageY)
+    }
     render() {
         return (
             <div>
                 <p>My name is: {this.state.name}</p>
                 <p>My age: {this.state.age}</p>
                 <p>My address: {this.state.address}</p>
-            </div>
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+            </div >
         );
     }
 }
