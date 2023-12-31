57a3f5a9c5d80fedfcbbed8858ad5a0d5e21e515
Đinh Mạnh Tưởng <145707598+prism9x@users.noreply.github.com>
2024-01-01 00:58:30 +0700

    togle list user and if else command

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,21 +1,39 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+    state = {
+        isShowListUser: true
+    }
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
+
     render() {
 
         const { listUsers } = this.props
         console.log(listUsers)
         return (
             <div>
-                {
-                    listUsers.map((item) => {
-                        return (
-                            <div key={item.id}>
-                                <div>My name is {item.name}</div>
-                                <div>My age is {item.age}</div>
-                            </div>
-                        )
-                    })
+                <div>
+                    <span onClick={() => { this.handleShowHide() }}>
+                        {this.state.isShowListUser === true ? "Hide list user" : "Hide List User"}
+                    </span>
+                </div>
+                {this.state.isShowListUser &&
+                    <div>
+                        {
+                            listUsers.map((item, index) => {
+                                return (
+                                    <div key={item.id} className={+item.age > 25 ? "green" : "red"}>
+                                        <div>My name is {item.name}</div>
+                                        <div>My age is {item.age}</div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 }
             </div>
         )
