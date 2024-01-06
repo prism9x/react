fdaff202d8e9ed4553675a54a8fde5133509fe74
Đinh Mạnh Tưởng <145707598+prism9x@users.noreply.github.com>
2024-01-06 23:01:31 +0700

    User router dom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import logo from './logo.svg';
 import './App.scss';
 import Header from './components/Header/Header';
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className='app-container'>
       <Header />
+      <div>
+        testlink
+        <div>
+          <button>
+            <Link to={`users`}>Go to user page</Link>
+          </button>
+          <button>
+            <Link to={`admins`}>Go to admin page</Link>
+          </button>
+        </div>
+      </div>
     </div >
   )
 }
