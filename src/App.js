b35e25f137413801a6e4f219c0ffea01d2b0177d
Đinh Mạnh Tưởng <145707598+prism9x@users.noreply.github.com>
2024-01-06 13:53:45 +0700

    add nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.scss';
+import Header from './components/Header/Header';
 import React from 'react';
-import MyComponent from './components/MyComponent';
-
 
 const App = () => {
   return (
-    <div className='App'>
-      Hello world
-      <button className='btn btn-primary'>Hello</button>
+    <div className='app-container'>
+      <Header />
     </div >
   )
 }
